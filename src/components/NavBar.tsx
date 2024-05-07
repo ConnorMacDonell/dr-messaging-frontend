3588b4f86eccd24309bc6fcb13f8161b6def6367
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-07 19:30:26 +0900

    Add logo and links to navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  HStack,
+  Image,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import logo from "../assets/skull.jpg";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <HStack width="100vw" justify="space-between" padding={5}>
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
+      <Box>
+        <Menu>
+          <MenuButton as={Button} colorScheme="blue">
+            Profile
+          </MenuButton>
+          <MenuList>
+            <MenuGroup title="Profile">
+              <MenuItem>My Account</MenuItem>
+              <MenuItem>Payments </MenuItem>
+            </MenuGroup>
+            <MenuDivider />
+            <MenuGroup title="Help">
+              <MenuItem>Docs</MenuItem>
+              <MenuItem>FAQ</MenuItem>
+            </MenuGroup>
+          </MenuList>
+        </Menu>
+        <Link to="/login">
+          <Button colorScheme="gray">Login</Button>
+        </Link>
+      </Box>
+    </HStack>
+  );
 };
 
 export default NavBar;
