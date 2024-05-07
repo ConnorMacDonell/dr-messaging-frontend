f8fc67db74d998a085bff74ebedef54cabec1517
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-07 19:33:51 +0900

    Add signup route.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+    ],
   },
 ]);
 
