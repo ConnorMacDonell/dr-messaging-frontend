708e552f931d9067bd37195b08e9a3f54ade8e21
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-07 19:31:47 +0900

    Create signup page.

---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,11 @@
+import CreateUserForm from "../components/CreateUserForm";
+
+const SignupPage = () => {
+  return (
+    <>
+      <CreateUserForm />
+    </>
+  );
+};
+
+export default SignupPage;
