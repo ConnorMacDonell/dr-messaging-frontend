edff873a5f022c30f84152269ebe0e019be1441b
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-08 21:29:40 +0900

    Allow for user logout.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,11 +9,25 @@ import {
   MenuGroup,
   MenuItem,
   MenuList,
+  useToast,
 } from "@chakra-ui/react";
 import logo from "../assets/skull.jpg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const toast = useToast();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    toast({
+      title: "Logged out",
+      status: "info",
+      duration: 3000,
+      isClosable: true,
+    });
+    navigate("/");
+  };
   return (
     <HStack width="100vw" justify="space-between" padding={5}>
       <Link to="/">
@@ -36,9 +50,16 @@ const NavBar = () => {
             </MenuGroup>
           </MenuList>
         </Menu>
-        <Link to="/login">
-          <Button colorScheme="gray">Login</Button>
-        </Link>
+        {!localStorage.getItem("access_token") && (
+          <Link to="/login">
+            <Button colorScheme="gray">Login</Button>
+          </Link>
+        )}
+        {localStorage.getItem("access_token") && (
+          <Button colorScheme="gray" onClick={handleLogout}>
+            Logout
+          </Button>
+        )}
       </Box>
     </HStack>
   );
