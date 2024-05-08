ba842f9434dc74a756941d970f79566a7e847a77
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-08 20:27:29 +0900

    Allow user login and store JWT in localStorage.

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  Button,
+  Center,
+  Input,
+  Spinner,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserCredentials } from "../entities/User";
+import { SubmitHandler, useForm } from "react-hook-form";
+import authService from "../services/authService";
+import AuthToken from "../entities/AuthToken";
+
+const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const toast = useToast();
+  const { register, handleSubmit } = useForm<UserCredentials>();
+
+  const onSubmit: SubmitHandler<UserCredentials> = async (d) => {
+    try {
+      setIsLoading(true);
+      const response: AuthToken = await authService.post(d);
+      localStorage.setItem("access_token", response.access_token);
+      localStorage.setItem("refresh_token", response.refresh_token);
+      setIsLoading(false);
+
+      toast({
+        title: "Login successful",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+
+      navigate(`/dashboard`);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Center>
+        <Box width="33vw">
+          Login
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label></label>
+            <Input
+              {...register("email", { required: "Email is required." })}
+              placeholder="Email"
+              type="email"
+              variant="filled"
+              borderRadius={4}
+              marginBottom={3}></Input>
+            <Input
+              {...register("password", { required: "Password is required." })}
+              placeholder="Password"
+              type="password"
+              variant="filled"
+              borderRadius={4}
+              marginBottom={3}></Input>
+            <Button type="submit">{isLoading ? <Spinner /> : "Login"}</Button>
+          </form>
+        </Box>
+      </Center>
+    </>
+  );
+};
+
+export default LoginForm;
