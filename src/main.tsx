8d14d24438964908f5b846efbbfe624e97ba4eab
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-09 18:01:46 +0900

    Set up QueryProvider in main.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes.tsx";
+import router from "./routing/routes.tsx";
 import "./index.css";
+
+const queryClient = new QueryClient();
+
+//TODO setup https https://web.dev/articles/enable-https
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript
-        initialColorMode={theme.config.initialColorMode}
-      ></ColorModeScript>
-      <RouterProvider router={router} />
+      <ColorModeScript initialColorMode="dark"></ColorModeScript>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
