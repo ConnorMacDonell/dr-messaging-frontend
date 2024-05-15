a2403df9a9676a5b17f216e074e0757eb72c97fe
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-15 11:58:26 +0900

    Add token parameter to useUser.

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query"
 import userService from "../services/usersService"
+import AuthToken from "../entities/AuthToken";
 
-const useUser = (data: string) => useQuery({
+const useUser = (data: string, token: AuthToken) => useQuery({
   queryKey: ['users', data],
-  queryFn: () => userService.get(data)
+  queryFn: () => userService.get(data, token)
 })
 
 export default useUser;
