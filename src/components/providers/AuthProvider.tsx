0f75c484291d5907f7cab80cb6b1b00d7a238f15
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-15 11:55:22 +0900

    Update AuthProvider, define onLogin behavior, add currentUserId to authContextType.

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -1,6 +1,9 @@
 import { useToast } from "@chakra-ui/react";
 import React, { createContext, useState } from "react";
-import { UserJwtPayload } from "jwt-decode";
+import { JwtPayload, UserJwtPayload, jwtDecode } from "jwt-decode";
+import AuthToken from "../../entities/AuthToken";
+import authService from "../../services/authService";
+import { UserCredentials } from "../../entities/User";
 
 declare module "jwt-decode" {
   export interface UserJwtPayload extends JwtPayload {
@@ -15,20 +18,46 @@ interface Props {
 }
 
 interface AuthContextType {
-  token: UserJwtPayload | null;
-  onLogin: () => Promise<void>;
+  token: AuthToken;
+  currentUserId: string;
+  onLogin: (data: UserCredentials) => Promise<void>;
   onLogout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 const AuthProvider = ({ children }: Props) => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState({ accessToken: "", refreshToken: "" });
+  const [currentUserId, setCurrentUserId] = useState("");
   const toast = useToast();
-  const handleLogin = async () => {};
+
+  const handleLogin = async (data: UserCredentials) => {
+    try {
+      const response: AuthToken = await authService.post(data);
+      setToken(response);
+
+      const { userId } = jwtDecode(response.accessToken) as UserJwtPayload;
+      setCurrentUserId(userId);
+
+      toast({
+        title: "Login successful",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        title: "Login failed",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
   const handleLogout = () => {
-    setToken(null);
+    setToken({ accessToken: "", refreshToken: "" });
+    setCurrentUserId("");
     toast({
       title: "Logged out",
       status: "info",
@@ -39,6 +68,7 @@ const AuthProvider = ({ children }: Props) => {
 
   const value = {
     token,
+    currentUserId,
     onLogin: handleLogin,
     onLogout: handleLogout,
   };
