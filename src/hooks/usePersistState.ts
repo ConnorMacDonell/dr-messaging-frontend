4601a93a3b4a9174b5c948c069974d5da14b5c93
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-17 10:47:48 +0900

    Create usePersistState hook to read and save states from/to localStorage.

--- src/hooks/usePersistState.ts
+++ src/hooks/usePersistState.ts
@@ -1,20 +1,20 @@
-import {useState, useEffect} from 'react';
-import TypedLocalStorage from '../util/TypedLocalStorage';
+import {useState} from 'react';
+
+const jsonifyNonStrings = (state: any) => {
+  return typeof state !== "string" ? JSON.stringify(state) : state;
+}
 
 export default <T>(storageKey: string, initialState: T) => {
-  const [state, setInternalState] = useState(initialState);
-  const typedLocalStorage = new TypedLocalStorage<T>();
 
-  useEffect(() =>{
-    const storedValue = typedLocalStorage.getItem(storageKey);
-    if(storedValue) {
-      setInternalState(storedValue);
-    }
-  }, []);
+  const storedValue = localStorage.getItem(storageKey);
+  const stringInitialState = jsonifyNonStrings(initialState);
+  const initializeWithState = storedValue ? storedValue : stringInitialState;
+  const [state, setInternalState] = useState(initializeWithState);
 
   const setState = (newState: T) => {
-    typedLocalStorage.setItem(storageKey, newState);
-    setInternalState(newState);
+    const jsonNewState = jsonifyNonStrings(newState);
+    localStorage.setItem(storageKey, jsonNewState);
+    setInternalState(jsonNewState);
   }
 
   return [state, setState] as const;
