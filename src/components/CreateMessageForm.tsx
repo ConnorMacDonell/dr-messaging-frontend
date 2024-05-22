68796fbcfb19a9ca532883c2665749defc98f572
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-22 11:17:10 +0900

    Make CreateMessageForm.

--- src/components/CreateMessageForm.tsx
+++ src/components/CreateMessageForm.tsx
@@ -1,5 +1,112 @@
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  Center,
+  Input,
+  Spinner,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import FormLabel from "./FormLabel";
+import { CreateMessageObject } from "../entities/Message";
+import { useToast } from "@chakra-ui/react";
+import messageService from "../services/messageService";
+
 const CreateMessageForm = () => {
-  return <div>CreateMessageForm</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit } = useForm<CreateMessageObject>();
+  const toast = useToast();
+
+  const onSubmit: SubmitHandler<CreateMessageObject> = async (d) => {
+    try {
+      setIsLoading(true);
+      await messageService.post(d);
+      setIsLoading(false);
+
+      toast({
+        title: "Message created",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error: any) {
+      setIsLoading(false);
+      const description =
+        error.code === "ERR_NETWORK"
+          ? "Network error, please try again later."
+          : error?.response?.data?.error;
+      toast({
+        title: "Message failed",
+        description: description,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      <AbsoluteCenter>
+        <Box
+          borderRadius="md"
+          borderWidth={1}
+          borderColor="black"
+          width="50vw"
+          boxShadow="10px 5px 5px gray">
+          <VStack>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormLabel
+                title="Create Message"
+                message="Please enter category and message"
+              />
+              <Input
+                {...register("category", {
+                  required: "Category is required.",
+                })}
+                placeholder="Gastro, Cataract, Ortho..."
+                type="string"
+                variant="filled"
+                borderRadius={4}
+                marginBottom={3}
+                autoFocus></Input>
+              <Input
+                {...register("message", {
+                  required: "Message is required.",
+                })}
+                placeholder="Hello, my name is Dr. Jones I will..."
+                type="string"
+                variant="filled"
+                borderRadius={4}
+                marginBottom={3}
+                pr="4.5rem"></Input>
+              <Center>
+                <VStack>
+                  <Button
+                    type="submit"
+                    borderRadius="md"
+                    borderWidth={0}
+                    bg="#000080"
+                    _hover={{
+                      bg: "white",
+                      textColor: "black",
+                      borderWidth: "1px",
+                    }}
+                    variant="ghost"
+                    marginBottom={7}
+                    textColor="White">
+                    {isLoading ? <Spinner /> : "Create Message"}
+                  </Button>
+                </VStack>
+              </Center>
+            </form>
+          </VStack>
+        </Box>
+      </AbsoluteCenter>
+    </>
+  );
 };
 
 export default CreateMessageForm;
