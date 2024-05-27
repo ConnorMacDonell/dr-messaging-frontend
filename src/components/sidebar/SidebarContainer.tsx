63ef27d08068b5e81d19a5ea7dbdf931d0a67588
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-27 15:43:51 +0900

    Increase sidebar size to 18vw.

--- src/components/sidebar/SidebarContainer.tsx
+++ src/components/sidebar/SidebarContainer.tsx
@@ -19,7 +19,7 @@ const SidebarContainer = ({ children, sidebar }: Props) => {
         <Box
           pos="sticky"
           top={0}
-          width={{ base: 0, md: "15vw" }}
+          width={{ base: 0, md: "18vw" }}
           height={{ base: 0, md: "100%" }}
           padding={{ base: 0, md: 2 }}
           paddingTop={8}
